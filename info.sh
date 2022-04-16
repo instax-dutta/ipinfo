033e86469061b3519aa2664b6c24b07478df7738
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2022-04-16 21:29:38 +0530

    Add files via upload

---
+++ info.sh
@@ -0,0 +1,19 @@
+echo #########################################################################
+echo #                                                                        #
+echo #                          Ip Info Fetcher                               #
+echo #                                                                        #
+echo #########################################################################
+echo
+echo "Enter the IP address you want to know the info of:"
+read ip
+echo
+echo "The IP address you entered is: $ip"
+echo "Thanks for using this script !!"
+echo "Ip Info Fetcher has fetched the folowing info:" 
+echo
+echo
+echo
+echo ########################################################
+curl ipinfo.io/$ip\?token=4b3569fd2ba9da
+echo ########################################################
+
